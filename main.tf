c3ea80fe48acc66c688b94a8907f7e21fe0b9a00
michaelpitts <michael@costa-pitts.com>
2026-02-05 09:29:34 -0800

    add variables

--- main.tf
+++ main.tf
@@ -1,42 +1,48 @@
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = "main-rg"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Virtual Network
 resource "azurerm_virtual_network" "main" {
-  name                = "main-vnet"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  tags = {
-    Name = "main-vnet"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.vnet_name
+    }
+  )
 }
 
 # Public Subnet
 resource "azurerm_subnet" "public" {
-  name                 = "public-subnet"
+  name                 = var.public_subnet_name
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = var.public_subnet_prefix
 }
 
 # Public IP
 resource "azurerm_public_ip" "main" {
-  name                = "main-public-ip"
+  name                = var.public_ip_name
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
-  tags = {
-    Name = "main-public-ip"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.public_ip_name
+    }
+  )
 }
 
 # Network Security Group
 resource "azurerm_network_security_group" "instance" {
-  name                = "instance-nsg"
+  name                = var.nsg_name
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -51,14 +57,17 @@ resource "azurerm_network_security_group" "instance" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  tags = {
-    Name = "instance-nsg"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.nsg_name
+    }
+  )
 }
 
 # Network Interface
 resource "azurerm_network_interface" "main" {
-  name                = "main-nic"
+  name                = var.nic_name
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -69,9 +78,12 @@ resource "azurerm_network_interface" "main" {
     public_ip_address_id          = azurerm_public_ip.main.id
   }
 
-  tags = {
-    Name = "main-nic"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.nic_name
+    }
+  )
 }
 
 # Network Interface Security Group Association
@@ -82,35 +94,38 @@ resource "azurerm_network_interface_security_group_association" "main" {
 
 # Virtual Machine
 resource "azurerm_linux_virtual_machine" "main" {
-  name                = "main-vm"
+  name                = var.vm_name
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
+  size                = var.vm_size
+  admin_username      = var.admin_username
 
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
 
   admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
   }
 
   os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_account_type
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
   }
-  tags = {
-    Name = "main-instance"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.vm_name
+    }
+  )
 }
 
 # Outputs
